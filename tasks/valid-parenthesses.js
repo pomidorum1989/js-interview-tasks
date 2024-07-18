0f1eb53ac9dcc1adfbedef2a29e1a281331793d7
Pomidorum <pomidorum1989@gmail.com>
2024-07-17 23:48:44 -0400

    Valid parenthesses update

--- tasks/valid-parenthesses.js
+++ tasks/valid-parenthesses.js
@@ -1,4 +1,5 @@
-var isValid = function (s) {
+ function isParenthesisValid(s) {
+    console.log(`Input value: ${s}`)
     const stack = [];
     const map = {
         '(': ')',
@@ -8,11 +9,18 @@ var isValid = function (s) {
     for (let char of s) {
         if (char in map) {
             stack.push(char);
-        } else {
+        } else if (Object.values(map).includes(char)) {
             if (stack.length === 0 || map[stack.pop()] !== char) {
+                console.log("Incorrect parenthesis order");
                 return false;
             }
         }
     }
+    console.log("Parenthesses order is correct")
     return stack.length === 0;
 };
+
+isParenthesisValid("}{");
+isParenthesisValid("{qwerty}");
+isParenthesisValid("}qwerty{");
+isParenthesisValid("{}");
